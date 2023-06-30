2a38e2cd772d98ced2169d48463f1ce8dd95f266
joonvent <gventura31@icloud.com>
2023-06-30 00:51:26 -0400

    First Unit Test

--- src/test/java/org/yearup/data/mysql/MySqlCategoryDaoTest.java
+++ src/test/java/org/yearup/data/mysql/MySqlCategoryDaoTest.java
@@ -1,12 +1,55 @@
 package org.yearup.data.mysql;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import org.yearup.models.Category;
+
+
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MySqlCategoryDaoTest {
+class MySqlCategoryDaoTest extends BaseDaoTestClass{
 
-    //error handling for each method i may copy from products
-    //test
+    private MySqlCategoryDao dao;
+    @BeforeEach
+    public void setup()
+    {
+        dao = new MySqlCategoryDao(dataSource, dataSource);
+    }
+    @Test
+    void getById_shouldReturn_theCorrectCategory() {
+        //arrange
+        int categoryId = 1;
+        Category expected = new Category();
+        expected.setCategoryId(categoryId);
 
-    
+          //act
+            Category actual = dao.getById(categoryId);
 
+
+                //assert
+        assertNotNull(actual, "The returned category should not be null.");
+        assertEquals(expected.getCategoryId(), actual.getCategoryId());
+            }
+
+
+
+
+    @Test
+    void delete() {
+
+        //arrange
+
+
+
+        //act
+
+
+
+        ///assert
+
+
+    }
 }
